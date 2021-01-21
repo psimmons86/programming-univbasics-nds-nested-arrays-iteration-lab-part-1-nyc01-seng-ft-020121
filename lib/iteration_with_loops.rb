2b4e071e68b7caffefc8c4f0290583094885f91b
patrick simmons <hadroncollides@gmail.com>
2021-01-21 18:56:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,7 @@ def find_even_values(src)
     [99, 50, 3, 4],
     [23,41]
     ]
-  
-def find_even_values(src)
+ def find_even_values(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
